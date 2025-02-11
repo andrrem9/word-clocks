f9711e81f4ce14d7c678bd9f5afb8a30bdba13b7
andrrem9 <moldoveanuandreea9@yahoo.com>
2025-02-11 00:20:13 +0000

    revised

--- javascript/index.js
+++ javascript/index.js
@@ -1,35 +1,38 @@
 function updateTime() {
+  // Los Angeles
   let losAngelesElement = document.querySelector("#los-angeles");
   if (losAngelesElement) {
     let losAngelesDateElement = losAngelesElement.querySelector(".date");
     let losAngelesTimeElement = losAngelesElement.querySelector(".time");
     let losAngelesTime = moment().tz("America/Los_Angeles");
 
-    losAngelesDateElement.innerHTML = losAngelesTime.format("MMM Do YYYY");
+    losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM	Do YYYY");
     losAngelesTimeElement.innerHTML = losAngelesTime.format(
-      "h:mm:ss [<small>]AM[</small>]"
+      "h:mm:ss [<small>]A[</small>]"
     );
   }
+
+  // Paris
   let parisElement = document.querySelector("#paris");
   if (parisElement) {
     let parisDateElement = parisElement.querySelector(".date");
     let parisTimeElement = parisElement.querySelector(".time");
     let parisTime = moment().tz("Europe/Paris");
 
-    parisDateElement.innerHTML = parisTime.format("MMM Do YYYY");
+    parisDateElement.innerHTML = parisTime.format("MMMM	Do YYYY");
     parisTimeElement.innerHTML = parisTime.format(
-      "h:mm:ss [<small>]AM[</small>]"
+      "h:mm:ss [<small>]A[</small>]"
     );
   }
 }
+
 function updateCity(event) {
   let cityTimeZone = event.target.value;
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML = cityTimeZone;
-  citiesElement = `
- <div class="city">
+  citiesElement.innerHTML = `
+  <div class="city">
     <div>
       <h2>${cityName}</h2>
       <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
@@ -40,7 +43,9 @@ function updateCity(event) {
   </div>
   `;
 }
+
 updateTime();
 setInterval(updateTime, 1000);
+
 let citiesSelectElement = document.querySelector("#city");
 citiesSelectElement.addEventListener("change", updateCity);
